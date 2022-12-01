cffe894ba569a169d923c93689690227e864fd33
Ananth080402 <118560005+Ananth080402@users.noreply.github.com>
2022-12-01 10:18:22 +0530

    Add files via upload

---
+++ noteApp.js
@@ -0,0 +1,54 @@
+const addTitle = document.getElementById('addTitle');
+const addText = document.getElementById('addText');
+const addNoteButton = document.getElementById('addNote');
+const notesDiv = document.getElementById('notes');
+let notes=[];
+
+function addNotes()
+{
+    let notes=localStorage.getItem('notes');
+    if(notes === null)
+     {
+        return;
+     }
+     else{
+        notes=JSON.parse(notes);
+     }
+   
+    if(addText.value=='')
+    {
+       alert('Add your note')
+       return;
+    }
+    const noteObj={
+        title:addTitle.value,
+        text:addText.value,
+    }
+    notes.push(noteObj);
+    localStorage.setItem('notes',JSON.stringify(notes)); 
+    showNotes();
+}
+
+function showNotes(){
+    let notesHTML='';
+    let notes=localStorage.getItem('notes');
+    if(notes === null)
+     {
+        return;
+     }
+     else{
+        notes=JSON.parse(notes);
+     }
+    for(let i=0; i<notes.length; i++){
+        console.log(notes[i])
+        notesHTML += `<div class="note">
+                         <button>Delete</button>
+                         <div class="title">${notes[i].title}</div>
+                         <div class="text">${notes[i].text}</div>
+                      </div>
+        `
+    }
+    notesDiv.innerHTML =notesHTML;
+
+}
+addNoteButton.addEventListener('click',addNotes);
